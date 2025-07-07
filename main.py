2b907351f0f64b229735be0acf0cd3cb43ce9c54
Aashish Regmi <regmiaashish660@gmail.com>
2025-07-07 14:23:55 +0545

    Add the gitignore

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# main.py
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def main():
-    return {'data':{'name':'Aashish Regmi'}}
+    return {'data': {'name': 'Aashish Regmi'}}
+
+@app.get("/about")
+def main():
+    return {'data': {'name': 'About us section'}}
