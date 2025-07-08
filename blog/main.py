5eb92d00038f71d8d37a1e65a02329de2780f58f
Aashish Regmi <regmiaashish660@gmail.com>
2025-07-08 19:57:05 +0545

    add the database and perform crud

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from . import schemas, models
+from .database import engine, get_db
+
+# Create all tables in the database
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
-@app.post('/blog')
-def create(title, body):
-    return {'title':title, 'body': body}
+
+@app.post('/blog', response_model=schemas.Blog)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def get_all_blog(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
